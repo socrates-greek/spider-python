39eda281dfbf62c52762e60a62944bfea3e486fc
Esonwang0923 <15209828293@163.com>
2018-05-04 22:25:29 +0800

    ff

---
+++ godzilla/src/main/java/com/godzilla/cn/godzilla/GodzillaApplication.java
@@ -0,0 +1,12 @@
+package com.godzilla.cn.godzilla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GodzillaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GodzillaApplication.class, args);
+    }
+}
---
+++ godzilla/src/test/java/com/godzilla/cn/godzilla/GodzillaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.godzilla.cn.godzilla;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GodzillaApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
